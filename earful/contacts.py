76ea4f14666a6437877062627fc119f69555f2af
Alec Elton <alec.elton@gmail.com>
2016-02-04 18:18:45 -0600

    Tests for contacts

--- earful/contacts.py
+++ earful/contacts.py
@@ -8,8 +8,8 @@ class ContactInformation(object):
 
 
 class EmailAddress(ContactInformation):
-    def __init__(self, email, *args, **kwargs):
-        super(EmailAddress, self).__init__(*args, **kwargs)
+    def __init__(self, name, email, *args, **kwargs):
+        super(EmailAddress, self).__init__(name, *args, **kwargs)
         self.email = email
 
     def __str__(self):
@@ -17,8 +17,8 @@ class EmailAddress(ContactInformation):
 
 
 class PhoneNumber(ContactInformation):
-    def __init__(self, phone, sms_ok=True, voice_ok=False, *args, **kwargs):
-        super(PhoneNumber, self).__init__(*args, **kwargs)
+    def __init__(self, name, phone, sms_ok=True, voice_ok=False, *args, **kwargs):
+        super(PhoneNumber, self).__init__(name, *args, **kwargs)
         self.phone = phone
         self.sms_ok = sms_ok
         self.voice_ok = voice_ok
@@ -33,8 +33,8 @@ class PhoneNumber(ContactInformation):
 
 
 class HipChat(ContactInformation):
-    def __init__(self, room, username=None, mention=None, notify=None, server=None, *args, **kwargs):
-        super(HipChat, self).__init__(*args, **kwargs)
+    def __init__(self, name, room, username=None, mention=None, notify=None, server=None, *args, **kwargs):
+        super(HipChat, self).__init__(name, *args, **kwargs)
         self.room = room
         self.username = username
         self.server = server
@@ -75,23 +75,24 @@ class Recipient(object):
 
     def contacts(self, of_type=None, include_all=False, **having):
         returned_types = []
-        for contact in sorted(self.contacts, key=lambda v: v.weight):
+        for contact in sorted(self._contacts, key=lambda v: v.weight):
             if of_type and not isinstance(contact, of_type):
                 continue
             for key, value in having.items():
                 if getattr(contact, key, None) != value:
+                    break
+            else:
+                if not include_all and contact.__class__ in returned_types:
                     continue
-            if not include_all and contact.__class__ in returned_types:
-                continue
-            returned_types.append(contact.__class__)
-            yield contact
+                returned_types.append(contact.__class__)
+                yield contact
 
 
 class Group(object):
     def __init__(self, name, groups=None, recipients=None, *args, **kwargs):
         self.name = name
         self._groups = groups or []
-        self._recipients recipients or []
+        self._recipients = recipients or []
 
     def groups(self, recursive=True):
         for group in self._groups:
@@ -101,11 +102,12 @@ class Group(object):
                     yield group_
 
     def recipients(self, recursive=True):
-        for recipient in self.recipients:
+        for recipient in self._recipients:
             yield recipient
-        for group in self.groups(recursive=False):
-            for recipient in group.recipients(recursive=recursive):
-                yield recipient
+        if recursive:
+            for group in self.groups(recursive=False):
+                for recipient in group.recipients(recursive=True):
+                    yield recipient
 
     def contacts(self, of_type=None, include_all=False, recursive=True, **having):
         for recipient in self.recipients(recursive=recursive):
--- tests/test_contacts.py
+++ tests/test_contacts.py
@@ -1,10 +1,108 @@
 from unittest import TestCase
 
 from earful.contacts import (
+    EmailAddress,
+    PhoneNumber,
     HipChat,
+    Recipient,
+    Group,
     )
 
 
-# class ContactInformationTest(TestCase):
-#     def test_hipchat(self):
-#         instance = HipChat
+class ContactInformationTest(TestCase):
+    def test_hipchat_defaults(self):
+        instance = HipChat('contactname', 'roomname')
+        self.assertEqual(instance.name, 'contactname')
+        self.assertEqual(instance.weight, 100)
+        self.assertEqual(instance.room, 'roomname')
+        self.assertTrue(instance.notify)
+        self.assertFalse(instance.mention)
+
+    def test_hipchat_withuser(self):
+        instance = HipChat('contactname', 'roomname', username='person')
+        self.assertFalse(instance.notify)
+        self.assertTrue(instance.mention)
+
+    def test_hipchat_setprefs(self):
+        instance = HipChat('contactname', 'roomname', username='person', notify=True, mention=False)
+        self.assertTrue(instance.notify)
+        self.assertFalse(instance.mention)
+
+
+class RecipientTest(TestCase):
+    def test_recipient_defaults(self):
+        r = Recipient('recipientname')
+        self.assertEqual(list(r.contacts()), [])
+
+    def test_simple_recipient(self):
+        c = [EmailAddress('emailname', 'emailaddr')]
+        r = Recipient('recipientname', contacts=c)
+        self.assertEqual(list(r.contacts()), c)
+
+    def test_less_simple_recipient(self):
+        c = [
+            EmailAddress('emailname', 'emailaddr'),
+            PhoneNumber('phonename', 'phonenum'),
+        ]
+        r = Recipient('recipientname', contacts=c)
+        self.assertEqual(list(r.contacts()), c)
+
+    def test_contacts_by_type(self):
+        c = [
+            EmailAddress('emailname', 'emailaddr'),
+            PhoneNumber('phonename', 'phonenum'),
+        ]
+        r = Recipient('recipientname', contacts=c)
+        self.assertEqual(list(r.contacts(of_type=EmailAddress)), [c[0]])
+
+    def test_contacts_with_weight(self):
+        c = [
+            EmailAddress('emailname', 'emailaddr'),
+            EmailAddress('emailname', 'emailaddr', weight=50),
+            PhoneNumber('phonename', 'phonenum'),
+        ]
+        r = Recipient('recipientname', contacts=c)
+        self.assertEqual(list(r.contacts()), c[1:])
+
+    def test_contacts_with_weight_all(self):
+        c = [
+            EmailAddress('emailname', 'emailaddr'),
+            EmailAddress('emailname', 'emailaddr', weight=50),
+            PhoneNumber('phonename', 'phonenum'),
+        ]
+        r = Recipient('recipientname', contacts=c)
+        self.assertEqual(list(r.contacts(include_all=True)), [c[1], c[0], c[2]])
+
+    def test_contacts_with_weight_type(self):
+        c = [
+            EmailAddress('emailname', 'emailaddr'),
+            EmailAddress('emailname', 'emailaddr', weight=50),
+            PhoneNumber('phonename', 'phonenum'),
+        ]
+        r = Recipient('recipientname', contacts=c)
+        self.assertEqual(list(r.contacts(of_type=EmailAddress)), [c[1]])
+
+    def test_contacts_having(self):
+        c = [
+            PhoneNumber('phonename', 'phonenum', sms_ok=False),
+            PhoneNumber('phonename', 'phonenum', sms_ok=True),
+        ]
+        r = Recipient('recipientname', contacts=c)
+        self.assertEqual(list(r.contacts(sms_ok=True)), [c[1]])
+
+
+class GroupTest(TestCase):
+    def test_groups(self):
+        t = EmailAddress('emailname', 'emailaddr')
+        r = Recipient('recipientname', contacts=[t])
+        c = Group('c', recipients=[r])
+        b = Group('b', groups=[c])
+        a = Group('a', groups=[b])
+
+        self.assertEqual(list(a.groups(recursive=False)), [b])
+        self.assertEqual(list(a.recipients(recursive=False)), [])
+        self.assertEqual(list(a.contacts(recursive=False)), [])
+
+        self.assertEqual(list(a.groups()), [b, c])
+        self.assertEqual(list(a.recipients()), [r])
+        self.assertEqual(list(a.contacts()), [t])
